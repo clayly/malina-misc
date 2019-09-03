607bfdfeedf76131544097629f5332f4bda0a922
clayly <clayly@gmail.com>
2019-09-03 13:24:48 +0300

    up

--- src/main/java/test/audio/App.java
+++ src/main/java/test/audio/App.java
@@ -6,69 +6,60 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 
-@SuppressWarnings({"ConstantConditions", "UnnecessaryLocalVariable", "SpellCheckingInspection"})
+@SuppressWarnings({"ConstantConditions", "UnnecessaryLocalVariable", "SpellCheckingInspection", "WeakerAccess"})
 public class App {
 
-    public static void main(String[] args) {
-        testFormats();
-//        AudioFormat systemFormat = systemFormat();
-//        AudioFormat mediumFormat = mediumFormat();
-//        AudioFormat linphoneFormat = linphoneFormat();
-//        System.out.println("systemFormat: " + systemFormat);
-//        System.out.println("mediumFormat: " + mediumFormat);
-//        System.out.println("linphoneFormat: " + linphoneFormat);
-//        System.out.println("isConversionSupported system to medium: " +
-//                AudioSystem.isConversionSupported(systemFormat, mediumFormat));
-//        System.out.println("isConversionSupported medium to linphone: " +
-//                AudioSystem.isConversionSupported(mediumFormat, linphoneFormat));
-//        Mixer.Info[] mixerInfo = AudioSystem.getMixerInfo();
-//        for (int i = 0; i < mixerInfo.length; ++i) {
-//            Mixer.Info info = mixerInfo[i];
-//            System.out.println();
-//            System.out.println(String.format(Locale.US,
-//                    "====== MIXER INFO num: %02d ====== \ndescr: <%s> \nname: <%s> \nvend: <%s> \nver: <%s>",
-//                    i,
-//                    info.getDescription(),
-//                    info.getName(),
-//                    info.getVendor(),
-//                    info.getVersion()
-//            ));
-//            Mixer mixer = AudioSystem.getMixer(info);
-//            Line.Info[] srcLinesInfo = mixer.getSourceLineInfo();
-//            System.out.println("srcLinesInfo cnt: " + srcLinesInfo.length);
-//            for (int i1 = 0; i1 < srcLinesInfo.length; i1++) {
-//                System.out.println(String.format(Locale.US,
-//                        "srcLineInfo num: %02d srcLineInfo: <%s>",
-//                        i1, srcLinesInfo[i1].toString()
-//                ));
-//            }
-//            Line.Info[] dstLinesInfo = mixer.getTargetLineInfo();
-//            System.out.println("dstLinesInfo cnt: " + dstLinesInfo.length);
-//            for (int i1 = 0; i1 < dstLinesInfo.length; i1++) {
-//                System.out.println(String.format(Locale.US,
-//                        "dstLineInfo num: %02d dstLineInfo: <%s>",
-//                        i1, dstLinesInfo[i1].toString()
-//                ));
-//            }
-//            TargetDataLine dst = null;
-//            SourceDataLine src = null;
-//            AudioFormat dstFormat = linphoneFormat();
-//            AudioFormat srcFormat = linphoneFormat();
-//            try {
-//                dst = AudioSystem.getTargetDataLine(dstFormat, info);
-//                System.out.println("dstDataLine OK " + dst.getLineInfo());
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//            try {
-//                src = AudioSystem.getSourceDataLine(srcFormat, info);
-//                System.out.println("srcDataLine OK " + src.getLineInfo());
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//            loopBack(dst, dstFormat, src, srcFormat);
-//            System.out.println("\n\n");
-//        }
+    static public final boolean LOG = true;
+    static public final boolean LOOPBACK_TEST = true;
+    static public final boolean FORMAT_TEST = false;
+
+    static private final AudioFormat.Encoding[] ENCODING = new AudioFormat.Encoding[]{
+            AudioFormat.Encoding.ALAW,
+            AudioFormat.Encoding.PCM_FLOAT,
+            AudioFormat.Encoding.PCM_SIGNED,
+            AudioFormat.Encoding.PCM_UNSIGNED,
+            AudioFormat.Encoding.ULAW};
+    static private final float[] SAMPLE_RATE = new float[]{8000, 16000, 32000};
+    static private final int[] SAMPLE_SIZE = new int[]{8, 16, 32};
+    static private final int[] CHANNELS = new int[]{1, 2};
+    static private final boolean[] BIG_ENDIAN = new boolean[]{true, false};
+
+    static private void log(String format, Object... args) {
+        if (LOG) System.out.println(String.format(Locale.US, format, args));
+    }
+
+    static private void print(String format, Object... args) {
+        System.out.println(String.format(Locale.US, format, args));
+    }
+
+    static private void print(int num, Mixer.Info info) {
+        System.out.println(String.format(Locale.US,
+                "====== MIXER INFO num: %02d ====== \ndescr: <%s> \nname: <%s> \nvend: <%s> \nver: <%s>",
+                num,
+                info.getDescription(),
+                info.getName(),
+                info.getVendor(),
+                info.getVersion()
+        ));
+    }
+
+    static public void main(String[] args) {
+        AudioFormat systemFormat = systemFormat();
+        AudioFormat mediumFormat = mediumFormat();
+        AudioFormat linphoneFormat = linphoneFormat();
+        log("systemFormat: " + systemFormat);
+        log("mediumFormat: " + mediumFormat);
+        log("linphoneFormat: " + linphoneFormat);
+        log("isConversionSupported system to medium: " +
+                AudioSystem.isConversionSupported(systemFormat, mediumFormat));
+        log("isConversionSupported medium to linphone: " +
+                AudioSystem.isConversionSupported(mediumFormat, linphoneFormat));
+        if (FORMAT_TEST) {
+            formatTest();
+        }
+        if (LOOPBACK_TEST) {
+            loopbackTest();
+        }
     }
 
     static private final long CHECK_PERIOD = 5000;
@@ -81,8 +72,8 @@ public class App {
             return;
         int frameSize = dstFormat.getFrameSize();
         int toRead = frameSize * READ_FACTOR;
-        dst.addLineListener(event -> System.out.println("dst: " + event.toString()));
-        src.addLineListener(event -> System.out.println("src: " + event.toString()));
+        dst.addLineListener(event -> log("dst: " + event.toString()));
+        src.addLineListener(event -> log("src: " + event.toString()));
         try {
 //            AudioInputStream dstStr = new AudioInputStream(dst);
 //            AudioInputStream srcStr = AudioSystem.getAudioInputStream(AudioFormat.Encoding.ULAW, dstStr);
@@ -109,7 +100,7 @@ public class App {
                 src.write(readData, 0, toRead);
                 byte[] readSlice = new byte[READ_SLICE];
                 System.arraycopy(readData, 0, readSlice, 0, readSlice.length);
-                System.out.println("read and written: " + readData.length + " slice: " + Arrays.toString(readSlice));
+                log("read and written: " + readData.length + " slice: " + Arrays.toString(readSlice));
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -119,60 +110,78 @@ public class App {
         }
     }
 
-    static private final AudioFormat.Encoding[] ENCODING = new AudioFormat.Encoding[]{
-            AudioFormat.Encoding.ALAW,
-            AudioFormat.Encoding.PCM_FLOAT,
-            AudioFormat.Encoding.PCM_SIGNED,
-            AudioFormat.Encoding.PCM_UNSIGNED,
-            AudioFormat.Encoding.ULAW};
-    static private final float[] SAMPLE_RATE = new float[]{8000, 16000, 32000};
-    static private final int[] SAMPLE_SIZE = new int[]{8, 16, 32};
-    static private final int[] CHANNELS = new int[]{1, 2};
-    static private final boolean[] BIG_ENDIAN = new boolean[]{true, false};
-
-    static private void testFormats() {
+    static private void loopbackTest() {
         Mixer.Info[] mixerInfo = AudioSystem.getMixerInfo();
         for (int i = 0; i < mixerInfo.length; ++i) {
             Mixer.Info info = mixerInfo[i];
-            System.out.println(String.format(Locale.US,
-                    "====== MIXER INFO num: %02d ====== \ndescr: <%s> \nname: <%s> \nvend: <%s> \nver: <%s>",
-                    i,
-                    info.getDescription(),
-                    info.getName(),
-                    info.getVendor(),
-                    info.getVersion()
-            ));
+            log("");
+            print(i, info);
+            Mixer mixer = AudioSystem.getMixer(info);
+            Line.Info[] srcLinesInfo = mixer.getSourceLineInfo();
+            log("srcLinesInfo cnt: " + srcLinesInfo.length);
+            for (int i1 = 0; i1 < srcLinesInfo.length; i1++)
+                log("srcLineInfo num: %02d srcLineInfo: <%s>", i1, srcLinesInfo[i1]);
+            Line.Info[] dstLinesInfo = mixer.getTargetLineInfo();
+            log("dstLinesInfo cnt: " + dstLinesInfo.length);
+            for (int i1 = 0; i1 < dstLinesInfo.length; i1++)
+                log("dstLineInfo num: %02d dstLineInfo: <%s>", i1, dstLinesInfo[i1]);
+            TargetDataLine dst = null;
+            SourceDataLine src = null;
+            AudioFormat dstFormat = linphoneFormat();
+            AudioFormat srcFormat = linphoneFormat();
+            try {
+                dst = AudioSystem.getTargetDataLine(dstFormat, info);
+                log("dstDataLine OK " + dst.getLineInfo());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            try {
+                src = AudioSystem.getSourceDataLine(srcFormat, info);
+                log("srcDataLine OK " + src.getLineInfo());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            loopBack(dst, dstFormat, src, srcFormat);
+            log("");
+        }
+    }
+
+    static private void formatTest() {
+        Mixer.Info[] mixerInfo = AudioSystem.getMixerInfo();
+        for (int i = 0; i < mixerInfo.length; ++i) {
+            Mixer.Info info = mixerInfo[i];
+            print(i, info);
             for (AudioFormat format : allFormats()) {
-//                System.out.println("check format: " + format);
+                log("check format: " + format);
                 boolean isDstOk = false;
                 try {
                     TargetDataLine dst = AudioSystem.getTargetDataLine(format, info);
-//                    System.out.println("OK dstDataLine getTargetDataLine lineInfo: " + dst.getLineInfo());
-//                    dst.addLineListener(event -> System.out.println("dstDataLine event: " + event));
+                    log("OK dstDataLine getTargetDataLine lineInfo: " + dst.getLineInfo());
+                    dst.addLineListener(event -> log("dstDataLine event: " + event));
                     dst.open(format);
-//                    System.out.println("OK dstDataLine open");
+                    log("OK dstDataLine open");
                     isDstOk = true;
                     dst.close();
                 } catch (Exception e) {
-//                    System.out.println("FAIL dstDataLine exception: " + e.getMessage());
+                    log("FAIL dstDataLine exception: " + e.getMessage());
                 }
                 boolean isSrcOk = false;
                 try {
                     SourceDataLine src = AudioSystem.getSourceDataLine(format, info);
-//                    System.out.println("OK srcDataLine getSourceDataLine lineInfo: " + src.getLineInfo());
-//                    src.addLineListener(event -> System.out.println("srcDataLine event: " + event));
+                    log("OK srcDataLine getSourceDataLine lineInfo: " + src.getLineInfo());
+                    src.addLineListener(event -> log("srcDataLine event: " + event));
                     src.open(format);
-//                    System.out.println("OK srcDataLine open");
+                    log("OK srcDataLine open");
                     isSrcOk = true;
                     src.close();
                 } catch (Exception e) {
-//                    System.out.println("FAIL srcDataLine exception: " + e.getMessage());
+                    log("FAIL srcDataLine exception: " + e.getMessage());
                 }
                 if (!isDstOk && !isSrcOk)
                     continue;
                 String dstStatus = isDstOk ? "OK" : "BAD";
                 String srcStatus = isSrcOk ? "OK" : "BAD";
-                System.out.println("dst " + dstStatus + " src " + srcStatus + " format: " + format);
+                print("dst " + dstStatus + " src " + srcStatus + " format: " + format);
             }
         }
     }
